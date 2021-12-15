7f9e57db2f6e59ec44fdb00efc328f6a1a60f354
Dinil V <dinil.vamanan@gmail.com>
2021-12-15 21:39:16 +0530

    Update solution.go

--- tower-of-hanoi/solution.go
+++ tower-of-hanoi/solution.go
@@ -1,1 +1,5 @@
 
+/* Tower of Hanoi, is a mathematical puzzle which consists of three towers (pegs) and more than one rings. 
+The puzzle starts with the disks in a neat stack in ascending order of size on one rod, the smallest at the top, thus making a conical shape. 
+Here is source code of the Go Program to implement Tower of Hanoi Algorithm.
+*/
